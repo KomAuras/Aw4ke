e466f964af5130b10d69f7b1b5a542b5c4162293
Евгений Стефаненко <mixer@clarionlife.net>
2020-08-28 16:22:21 +1000

    переписал все. бот транслирует сообщения по emoju RU

--- read.md
+++ read.md
@@ -1,3 +1,6 @@
+* Dev version lib.
+https://github.com/teamreflex/DiscordPHP/tree/develop
+
 * Add bot
 https://discordjs.guide/preparations/adding-your-bot-to-servers.html
 
@@ -9,3 +12,6 @@ https://discord.com/developers/applications
 
 * Assign bot ot server
 <a href="https://discord.com/oauth2/authorize?client_id=747281112295931995&scope=bot">Assign ChannelRelayBot</a>
+
+* Add service on Centos 6
+https://gist.github.com/KomAuras/4db94e6cda45650726f678cde5806c69
--- run.php
+++ run.php
@@ -2,42 +2,99 @@
 
 include __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$dotenv->required('token')->required();
-$dotenv->required('read_channel_id')->isInteger()->required();
-$dotenv->required('relay2webhookurl')->required();
+use Discord\DiscordCommandClient;
+use Discord\WebSockets\Event;
 
-$discord = new \Discord\Discord([
-    'token' => $_ENV['token']
-]);
-
-$discord->on('ready', function ($discord) {
-    $discord->on('message', function ($message) {
-        if ($message->channel_id == $_ENV['read_channel_id']) {
-            post2discord($message->content);
-        }
-    });
-});
-
-$discord->run();
-
-function post2discord($text)
+class MyBot
 {
-    $webhookurl = $_ENV['relay2webhookurl'];
-    $json_data = json_encode([
-        "content" => $text,
-        "username" => "ChannelRelayBot",
-    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    private $dotenv;
+    private $discord;
+    private $translated = [];
 
-    $ch = curl_init($webhookurl);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    function __construct()
+    {
+        $this->dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    }
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+    function Run()
+    {
+        $this->dotenv->load();
+        $this->dotenv->required('token')->required();
+
+        $this->discord = new DiscordCommandClient(['token' => $_ENV['token']]);
+
+        $this->discord->on('ready', function ($discord) {
+            $discord->on(Event::MESSAGE_REACTION_ADD, function ($message_reaction) {
+                if ($message_reaction->emoji->name == "🇷🇺") {
+                    if (!in_array($message_reaction->message_id, $this->translated)) {
+                        //echo file_put_contents('message_reaction.log', print_r($message_reaction, true)), PHP_EOL;
+                        $promise = $message_reaction->channel->getMessage($message_reaction->message_id);
+                        $promise->then(
+                            function ($response) {
+                                $this->getMessage($response);
+                            }
+                        );
+                        $this->translated[] = $message_reaction->message_id;
+                    }
+                }
+            });
+        });
+        $this->discord->run();
+    }
+
+    private function getMessage($message)
+    {
+        $text = $this->Lat2ru($message->content);
+        $message->channel->sendMessage($text);
+    }
+
+    private function Lat2ru($string)
+    {
+        $cyr = array(
+            "Щ", "Ш", "Ч", "Ц", "Ю", "Я", "Ж", "А", "Б", "В", "В",
+            "Г", "Д", "Е", "Ё", "З", "И", "Й", "К", "Л", "М", "Н",
+            "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ь", "Ы", "Ъ",
+            "Э", "Є", "Ї", "І",
+            "щ", "ш", "ч", "ц", "ю", "я", "ж", "а", "б", "в", "в",
+            "г", "д", "е", "ё", "з", "и", "й", "к", "л", "м", "н",
+            "о", "п", "р", "с", "т", "у", "ф", "х", "ь", "ы", "ъ",
+            "э", "є", "ї", "і"
+        );
+        $lat = array(
+            "Shch", "Sh", "Ch", "C", "Yu", "Ya", "J", "A", "B", "V", "W",
+            "G", "D", "E", "E", "Z", "I", "y", "K", "L", "M", "N",
+            "O", "P", "R", "S", "T", "U", "F", "H", "",
+            "Y", "", "E", "E", "Yi", "I",
+            "shch", "sh", "ch", "c", "Yu", "Ya", "j", "a", "b", "v", "w",
+            "g", "d", "e", "e", "z", "i", "y", "k", "l", "m", "n",
+            "o", "p", "r", "s", "t", "u", "f", "h",
+            "", "y", "", "e", "e", "yi", "i"
+        );
+        $string = str_replace($lat, $cyr, $string);
+        $string = str_replace("_", " ", $string);
+        return ($string);
+    }
+
+    private function post2discord($text)
+    {
+        $webhookurl = $_ENV['relay2webhookurl'];
+        $json_data = json_encode([
+            "content" => $text,
+            "username" => "ChannelRelayBot",
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+        $ch = curl_init($webhookurl);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+    }
 }
+
+$bot = new MyBot();
+$bot->Run();
